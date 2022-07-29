478482245eaaf5b62712706e4849a1b93dd6e140
Steven Richart <59703755+Miyuki62@users.noreply.github.com>
2022-07-29 15:38:37 +0200

    finition des page statique

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Error404 from "../pages/Error404";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import User from "../pages/User";
 
 const Router = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
-			{/* <Route path="/login" element={<SignIn />} /> */}
-			{/* <Route path="/profile" element={<User />} /> */}
+			<Route path="/login" element={<Login />} />
+			<Route path="/profile" element={<User />} />
 			{/* path="*" fonctionne si l'url n'est pas bonne */}
-			{/* <Route path="*" element={<Page404 />} /> */}
+			<Route path="*" element={<Error404 />} />
 		</Routes>
 	);
 };
